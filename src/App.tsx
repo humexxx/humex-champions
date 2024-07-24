1e4d2b39f0c51390d672f95bd700d62956fd558d
humexxx <jahume92@gmail.com>
2024-07-23 20:58:28 -0600

    chore: Add Vite configuration and TypeScript files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { SignInProps } from 'humex-auth/dist/types';
+import { SignIn } from 'humex-auth';
+
+function App() {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+}
+
+export default App;
