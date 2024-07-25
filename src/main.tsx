b6939ab0d0d6d19fef1dd37502056525868949ff
humexxx <jahume92@gmail.com>
2024-07-24 20:10:01 -0600

    feat: Refactor routing and add client dashboard page

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage.tsx';
+import ClientLayout from './layouts/ClientLayout.tsx';
+import SignInPage from './pages/SignInPage.tsx';
+import DashboardPage from './pages/client/DashboardPage.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
   },
   {
-    path: '/app',
-    element: <App />,
+    path: '/signin',
+    element: <SignInPage />,
+  },
+  {
+    path: '/client',
+    element: <ClientLayout />,
+    children: [
+      {
+        path: '/client/dashboard',
+        element: <DashboardPage />,
+      },
+    ],
   },
 ]);
 
