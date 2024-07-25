8f3d17f8bfd64b27d276a0637ae0ff1571e671b2
humexxx <jahume92@gmail.com>
2024-07-24 19:26:17 -0600

    feat: Add user sign up functionality using Firebase authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-import SignIn from './components/signin';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import SignIn, { SignInForm } from './components/signin';
+import { auth } from './firebase';
 
-async function handleOnSubmit() {}
+async function handleOnSubmit(form: SignInForm) {
+  await createUserWithEmailAndPassword(auth, form.email, form.password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+}
 
 function App() {
   return (
