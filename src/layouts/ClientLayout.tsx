b5a636a61ad04a587131ef02911e3753ca2bc76a
Jason Hume Gonzalez <74991185+humexxx@users.noreply.github.com>
2024-07-24 20:12:19 -0600

    Update react-router-dom to version 6.25.1 and refactor routing (#1)
    * chore: Update react-router-dom to version 6.25.1
    
    * feat: Refactor routing and add client dashboard page
    
    * feat: Add landing page component and update main route

---
+++ src/layouts/ClientLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+const ClientLayout = () => {
+  return (
+    <div>
+      ClientLayout
+      <Outlet />
+    </div>
+  );
+};
+
+export default ClientLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage.tsx';
+import ClientLayout from './layouts/ClientLayout.tsx';
+import SignInPage from './pages/SignInPage.tsx';
+import DashboardPage from './pages/client/DashboardPage.tsx';
+import LandingPage from './pages/LandingPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/signin',
+    element: <SignInPage />,
+  },
+  {
+    path: '/client',
+    element: <ClientLayout />,
+    children: [
+      {
+        path: '/client/dashboard',
+        element: <DashboardPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
