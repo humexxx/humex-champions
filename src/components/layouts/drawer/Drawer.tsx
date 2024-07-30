151b337c23fa722523ca4a319b19ebe0ffdd4ccf
humexxx <jahume92@gmail.com>
2024-07-29 21:03:28 -0600

    feat: Add responsive drawer component
    The code changes introduce a new `Drawer` component that implements a responsive drawer layout. It includes a toolbar, divider, and two lists with items. The `Drawer` component is imported and used in the `ClientLayout` component, replacing the previous implementation. The `DRAWER_WIDTH` constant is also exported from the `Drawer` module.
    
    Recent repository commits indicate a focus on updating components to use MUI v5, refactoring form handling and error handling, adding user authentication functionality, and updating routing.

---
+++ src/components/layouts/drawer/Drawer.tsx
@@ -0,0 +1,49 @@
+import {
+  Toolbar,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const Drawer = () => {
+  return (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {['Finanzas', 'Salud', 'Objetivos'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Miembros', 'Grupos'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export const DRAWER_WIDTH = 240;
+
+export default Drawer;
--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -1,34 +1,17 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+import LogoutIcon from '@mui/icons-material/Logout';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { PrivateRoute } from 'src/components/common';
 import { Outlet } from 'react-router-dom';
+import APP_DRAWER, { DRAWER_WIDTH } from 'src/components/layouts/drawer';
 
-const drawerWidth = 240;
-
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * Remove this when copying and pasting into your project.
-   */
-  window?: () => Window;
-}
-
-export default function ResponsiveDrawer(props: Props) {
-  const { window } = props;
+export default function ResponsiveDrawer() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
 
@@ -47,50 +30,16 @@ export default function ResponsiveDrawer(props: Props) {
     }
   };
 
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  // Remove this const when copying and pasting into your project.
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
   return (
     <PrivateRoute>
       <Box sx={{ display: 'flex' }}>
         <AppBar
           position="fixed"
           sx={{
-            width: { sm: `calc(100% - ${drawerWidth}px)` },
-            ml: { sm: `${drawerWidth}px` },
+            width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
+            ml: { sm: `${DRAWER_WIDTH}px` },
+            display: 'flex',
+            justifyContent: 'space-between',
           }}
         >
           <Toolbar>
@@ -106,16 +55,23 @@ export default function ResponsiveDrawer(props: Props) {
             <Typography variant="h6" noWrap component="div">
               Responsive drawer
             </Typography>
+            <IconButton
+              color="inherit"
+              edge="end"
+              onClick={() => console.log('Logout')} // Replace with actual logout function
+              sx={{ mr: 2 }}
+            >
+              <LogoutIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
         <Box
           component="nav"
-          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          sx={{ width: { sm: DRAWER_WIDTH }, flexShrink: { sm: 0 } }}
           aria-label="mailbox folders"
         >
           {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
           <Drawer
-            container={container}
             variant="temporary"
             open={mobileOpen}
             onTransitionEnd={handleDrawerTransitionEnd}
@@ -127,11 +83,11 @@ export default function ResponsiveDrawer(props: Props) {
               display: { xs: 'block', sm: 'none' },
               '& .MuiDrawer-paper': {
                 boxSizing: 'border-box',
-                width: drawerWidth,
+                width: DRAWER_WIDTH,
               },
             }}
           >
-            {drawer}
+            <APP_DRAWER />
           </Drawer>
           <Drawer
             variant="permanent"
@@ -139,12 +95,12 @@ export default function ResponsiveDrawer(props: Props) {
               display: { xs: 'none', sm: 'block' },
               '& .MuiDrawer-paper': {
                 boxSizing: 'border-box',
-                width: drawerWidth,
+                width: DRAWER_WIDTH,
               },
             }}
             open
           >
-            {drawer}
+            <APP_DRAWER />
           </Drawer>
         </Box>
         <Box
@@ -152,7 +108,7 @@ export default function ResponsiveDrawer(props: Props) {
           sx={{
             flexGrow: 1,
             p: 3,
-            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
           }}
         >
           <Toolbar />
