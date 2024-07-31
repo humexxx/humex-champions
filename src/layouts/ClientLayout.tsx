54fff67208e423eaa35807236789061985c53c64
Jason Hume Gonzalez <74991185+humexxx@users.noreply.github.com>
2024-07-30 18:24:04 -0600

    feat: Update theme context and provider (#5)
    The code changes introduce updates to the theme context and provider. The `ThemeProvider` component now accepts a `children` prop and wraps the `MuiThemeProvider` component. The `ThemeContext` is exported from the `ThemeContext` module. Additionally, the `ThemeContext.types` and `ThemeContext.hooks` are exported from the `ThemeContext` module. These changes improve the organization and structure of the theme-related code.

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -5,6 +5,8 @@ import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import LogoutIcon from '@mui/icons-material/Logout';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { PrivateRoute } from 'src/components/common';
@@ -12,9 +14,14 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import APP_DRAWER, { DRAWER_WIDTH } from 'src/components/layouts/drawer';
 import { auth } from 'src/firebase';
 
-export default function ResponsiveDrawer() {
+import { ThemeProvider, useThemeContext } from 'src/context/theme';
+import { useTheme } from '@mui/material/styles';
+
+function ClientLayout() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
+  const theme = useTheme();
+  const themeContext = useThemeContext();
 
   const handleDrawerClose = () => {
     setIsClosing(true);
@@ -45,91 +52,110 @@ export default function ResponsiveDrawer() {
   }
 
   return (
-    <PrivateRoute>
-      <Box sx={{ display: 'flex' }}>
-        <AppBar
-          position="fixed"
+    <Box sx={{ display: 'flex' }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
+          ml: { sm: `${DRAWER_WIDTH}px` },
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" flexGrow="1">
+            Champions
+          </Typography>
+          <IconButton
+            sx={{ ml: 1 }}
+            onClick={themeContext.toggleColorMode}
+            color="inherit"
+          >
+            {theme.palette.mode === 'dark' ? (
+              <Brightness4Icon />
+            ) : (
+              <Brightness7Icon />
+            )}
+          </IconButton>
+          <IconButton
+            color="inherit"
+            edge="end"
+            onClick={handleLogout}
+            sx={{ mr: 2 }}
+          >
+            <LogoutIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: DRAWER_WIDTH }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          ModalProps={{
+            keepMounted: true,
+          }}
           sx={{
-            width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
-            ml: { sm: `${DRAWER_WIDTH}px` },
-            display: 'flex',
-            justifyContent: 'space-between',
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: DRAWER_WIDTH,
+            },
           }}
         >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" flexGrow="1">
-              Champions
-            </Typography>
-            <IconButton
-              color="inherit"
-              edge="end"
-              onClick={handleLogout}
-              sx={{ mr: 2 }}
-            >
-              <LogoutIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Box
-          component="nav"
-          sx={{ width: { sm: DRAWER_WIDTH }, flexShrink: { sm: 0 } }}
-          aria-label="mailbox folders"
-        >
-          <Drawer
-            variant="temporary"
-            open={mobileOpen}
-            onTransitionEnd={handleDrawerTransitionEnd}
-            onClose={handleDrawerClose}
-            ModalProps={{
-              keepMounted: true,
-            }}
-            sx={{
-              display: { xs: 'block', sm: 'none' },
-              '& .MuiDrawer-paper': {
-                boxSizing: 'border-box',
-                width: DRAWER_WIDTH,
-              },
-            }}
-          >
-            <APP_DRAWER />
-          </Drawer>
-          <Drawer
-            variant="permanent"
-            sx={{
-              display: { xs: 'none', sm: 'block' },
-              '& .MuiDrawer-paper': {
-                boxSizing: 'border-box',
-                width: DRAWER_WIDTH,
-              },
-            }}
-            open
-          >
-            <APP_DRAWER />
-          </Drawer>
-        </Box>
-        <Box
-          component="main"
+          <APP_DRAWER />
+        </Drawer>
+        <Drawer
+          variant="permanent"
           sx={{
-            flexGrow: 1,
-            p: 3,
-            width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
-            bgcolor: 'grey.50',
-            minHeight: '100vh',
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: DRAWER_WIDTH,
+            },
           }}
+          open
         >
-          <Toolbar />
-          <Outlet />
-        </Box>
+          <APP_DRAWER />
+        </Drawer>
       </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
+          bgcolor: 'background.paper',
+          minHeight: '100vh',
+        }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+}
+
+export default function ClientLayoutWrapper() {
+  return (
+    <PrivateRoute>
+      <ThemeProvider>
+        <ClientLayout />
+      </ThemeProvider>
     </PrivateRoute>
   );
 }
