2e9eb4c7404f8067b54a6ba567a019a2abe948be
humexxx <jahume92@gmail.com>
2024-08-03 21:21:01 -0600

    feat: Add support for internationalization (i18n)
    The code changes introduce support for internationalization (i18n) in the application. The following updates are made:
    - Added new files: `src/locales/en.json` and `src/locales/es.json` to store translation resources for English and Spanish languages respectively.
    - Created `src/types/types.d.ts` and `src/types/resources.d.ts` to define types for translation resources.
    - Added `i18n.ts` file to initialize i18n with translation resources and language options.
    - Updated `Drawer.tsx` component to use translations for menu items.
    - Added `useTranslation` hook from `react-i18next` to translate text in components.
    
    These changes enable the application to support multiple languages and improve the user experience for international users.

---
+++ src/types/resources.d.ts
@@ -0,0 +1,26 @@
+interface Resources {
+  "en": {
+    "lng": "English",
+    "routes": {
+      "dashboard": "Dashboard",
+      "finances": "Finances",
+      "health": "Health",
+      "goals": "Goals",
+      "members": "Members",
+      "groups": "Groups"
+    }
+  },
+  "es": {
+    "lng": "Spanish",
+    "routes": {
+      "dashboard": "Panel",
+      "finances": "Finanzas",
+      "health": "Salud",
+      "goals": "Objetivos",
+      "members": "Miembros",
+      "groups": "Grupos"
+    }
+  }
+}
+
+export default Resources;
