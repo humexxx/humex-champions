9f645a02a84b2504b5ae5d912771b587129139c3
Jason Hume Gonzalez <74991185+humexxx@users.noreply.github.com>
2024-08-06 19:19:11 -0600

    feat: Add @mui/x-date-pickers and dayjs dependencies, and create TradingJournal components (#9)
    The code changes introduce the following updates:
    - Added "@mui/x-date-pickers" and "dayjs" dependencies to the package.json file.
    - Created the "TradingJournal" directory with the following files:
      - "index.ts" exports the "EditPanel" and "SummaryPanel" components, and the "Trade" and "SummaryPanelProps" interfaces.
      - "TradingJournal.types.ts" defines the "Trade" and "SummaryPanelProps" interfaces.
    - Updated the FinancesPage component to display the translated title for the trading journal section.
    - Added the "DateCalendar" component from "@mui/x-date-pickers/DateCalendar" to the TradingJournalPage component.
    - Created the "EditPanel" and "SummaryPanel" components in the "TradingJournal" directory.
    
    These changes add support for trading journal functionality and improve the user experience by displaying trading-related information and allowing users to edit and view summary data.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes.tsx';
 import { AuthProvider } from './context/auth';
 import { CssBaseline } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import './i18n.ts';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-  <AuthProvider>
-    <CssBaseline />
-    <RouterProvider router={router} />
-  </AuthProvider>
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <AuthProvider>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </LocalizationProvider>
   // </React.StrictMode>
 );
