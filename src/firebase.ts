965e98cca6813ce1a6edf1f9d0676ccc27019f23
humexxx <jahume92@gmail.com>
2024-10-11 14:45:13 -0600

    feat: secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,12 +8,12 @@ import { getFunctions } from 'firebase/functions';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAQB6UEYwnPtRlMNtcCCDV9rzyGH-mCGNw',
-  authDomain: 'humex-champions.firebaseapp.com',
-  projectId: 'humex-champions',
-  storageBucket: 'humex-champions.appspot.com',
-  messagingSenderId: '412486963304',
-  appId: '1:412486963304:web:907bb9f5f8764b58f76bdd',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
