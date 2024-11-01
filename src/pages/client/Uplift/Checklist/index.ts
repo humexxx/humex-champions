8f2560989b289ae5c0ccf41c9cecbdfc33df4ae1
Jason Hume Gonzalez <74991185+humexxx@users.noreply.github.com>
2024-10-31 23:08:47 -0500

    Feature/planner (#67)

--- src/pages/client/Uplift/Checklist/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ChecklistPage } from './Page';
---
+++ src/pages/client/Uplift/Planner/components/DailyChecklist.tsx
@@ -0,0 +1,106 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import {
+  Box,
+  Card,
+  CardContent,
+  Checkbox,
+  Chip,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { IPlanner } from '@shared/models/uplift';
+import { Dayjs } from 'dayjs';
+
+import Form from './Form';
+import { usePlannerSetter } from '../hooks';
+
+const DailyChecklist = ({
+  day,
+  data,
+}: {
+  day: Dayjs;
+  data?: IPlanner<Dayjs>;
+}) => {
+  const { set } = usePlannerSetter();
+
+  const handleOnChange = (index: number, checked: boolean) => () => {
+    if (!data) return;
+    data.items[index].completed = !checked;
+    set(data);
+  };
+
+  const handleOnDelete = (index: number) => () => {
+    if (!data) return;
+    data.items.splice(index, 1);
+    set(data);
+  };
+
+  return (
+    <Card variant={'outlined'} sx={{ minHeight: 500 }}>
+      <CardContent sx={{ p: 2 }}>
+        <Container maxWidth="xs" sx={{ marginLeft: 0 }}>
+          <Typography variant={'h6'}>
+            {day.format('dddd')}
+            <Typography component={'span'} variant={'body2'} sx={{ ml: 1 }}>
+              {day.format('MMM DD')}
+            </Typography>
+          </Typography>
+          <Box py={2}>
+            <Form planner={data ?? { date: day, items: [] }} />
+          </Box>
+          <List>
+            {data?.items.map((item, index) => (
+              <ListItem
+                key={index}
+                disablePadding
+                secondaryAction={
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={handleOnDelete(index)}
+                  >
+                    <DeleteIcon color="error" />
+                  </IconButton>
+                }
+              >
+                <ListItemButton
+                  role="button"
+                  onClick={handleOnChange(index, item.completed)}
+                  dense
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={item.completed}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': '1' }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={item.title}
+                    secondary={
+                      <Stack direction={'row'} gap={1}>
+                        <Chip label={'test'} />
+                        <Chip label={'test'} />
+                      </Stack>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Container>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DailyChecklist;
