8440bd2e6157fd27ff10c5be261bcad81c052256
humexxx <jahume92@gmail.com>
2025-02-06 18:06:30 -0700

    feat: add metricts card basic

--- src/pages/client/dashboardPage/Page.tsx
+++ src/pages/client/dashboardPage/Page.tsx
@@ -1,6 +1,9 @@
-import { Box, Container, Typography } from '@mui/material';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { Page } from 'src/components/layout';
+
+import { MetricsCard } from './_components';
 
 const DashboardPage = () => {
   const { t } = useTranslation(undefined, { keyPrefix: 'dashboard' });
@@ -16,25 +19,47 @@ const DashboardPage = () => {
             lg: 4,
           },
           mx: {
-            md: 0,
-            lg: 2,
+            sm: 0,
+            md: 2,
           },
         }}
       >
-        <Container maxWidth="md" sx={{ py: 4 }}>
+        <Container maxWidth="lg" sx={{ py: 4 }}>
           <Typography variant="h6" component="h1" color="common.white">
             Dashboard
           </Typography>
           <Box height={300}></Box>
         </Container>
       </Box>
-      <Box component={'section'}>
-        <Container maxWidth="md" sx={{ py: 4 }}>
-          <Typography variant="h6" component="h1">
-            Some other content
-          </Typography>
-          <Box height={300}></Box>
-        </Container>
+      <Box
+        sx={{
+          mt: -8,
+          mx: {
+            sm: 0,
+            md: 2,
+          },
+        }}
+      >
+        <Box component={'section'}>
+          <Container maxWidth="lg" sx={{ py: 4 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6} lg={4}>
+                <MetricsCard
+                  title="Finances"
+                  icon={<ShowChartIcon />}
+                  loading
+                  redirectPath="/client/finances"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} lg={4}>
+                <MetricsCard title="Page Views" />
+              </Grid>
+              <Grid item xs={12} sm={6} lg={4}>
+                <MetricsCard title="Bounce Rate" />
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
       </Box>
     </Page>
   );
